8487bdebb0658b1981734264e7d9ea9bd6dc643b
Robert Mikolajczyk <ronert1674@gmail.com>
2024-06-21 16:42:57 +0200

    add ship's speed adjustment

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,7 +23,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_screen()
-            self.clock.tick(60)
+            self.clock.tick(120)
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
