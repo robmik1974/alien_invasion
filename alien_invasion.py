6245ffee29e794adbabc33075ec0d67832066a5c
Robert Mikolajczyk <ronert1674@gmail.com>
2024-06-21 10:50:28 +0200

    refactor check events

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,15 +21,20 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
 
             pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == "__main__":
