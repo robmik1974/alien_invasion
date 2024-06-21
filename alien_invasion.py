263e92b70829558092c93b6bc90213055e089995
Robert Mikolajczyk <ronert1674@gmail.com>
2024-06-21 09:21:36 +0200

    add custom background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,7 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -20,6 +21,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
 
             pygame.display.flip()
             self.clock.tick(60)
