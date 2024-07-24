dffc9c64d1bca1a96eb80f3f56617ff1448063fe
Robert Mikolajczyk <ronert1674@gmail.com>
2024-07-24 16:40:18 +0200

    update check events

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,8 +39,10 @@ class AlienInvasion:
         """Respond to keypresses."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
-        if event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """Respond to keypresses."""
